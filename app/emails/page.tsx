0d23af93e0d19201009e2ad72b764f11b8fff202
ayodiya <devayodiya@gmail.com>
2024-05-22 07:00:25 +0100

    add email list page

---
+++ app/emails/page.tsx
@@ -0,0 +1,51 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+
+export default function Messages() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "50px",
+      }}
+    >
+      <Card sx={{ maxWidth: 345, backgroundColor: "secondary.main" }}>
+        <CardActionArea>
+          <CardContent>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box
+                sx={{
+                  color: "black",
+                  fontSize: "24px",
+                  fontWeight: 700,
+                }}
+              >
+                Lizard
+              </Box>
+            </Box>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species...
+            </Typography>
+            <Box
+              sx={{
+                paddingTop: "30px",
+              }}
+            >
+              fkfkffk
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Box>
+  );
+}
