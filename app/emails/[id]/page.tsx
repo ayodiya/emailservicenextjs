c30b9c13b34dcffd3a7af1e8f3080aee314f9b06
ayodiya <devayodiya@gmail.com>
2024-05-22 10:10:37 +0100

    add attach to api

--- app/emails/[id]/page.tsx
+++ app/emails/[id]/page.tsx
@@ -1,10 +1,52 @@
+"use client";
+
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import { ColorRing } from "react-loader-spinner";
+
+interface Message {
+  subject: string;
+  content: string;
+  sender: string;
+}
 
 export default function ReadEmail() {
+  const { id } = useParams<{ id: string }>();
+  const [message, setMessage] = useState<Message | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect((): void => {
+    (async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL_TEST}/api/message/message-read/${id}`,
+        );
+
+        if (!res.ok) {
+          Notify.failure(`Error fetching message: ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        setMessage({
+          subject: data?.messageExists?.subject,
+          content: data?.messageExists?.content,
+          sender: data?.messageExists?.sender,
+        });
+      } catch (error) {
+        Notify.failure("Failed to fetch email details");
+      }
+      setLoading(false);
+    })();
+  }, [id]);
+
   return (
     <Box
       sx={{
@@ -13,39 +55,51 @@ export default function ReadEmail() {
         paddingTop: "50px",
       }}
     >
-      <Card sx={{ maxWidth: 345, backgroundColor: "secondary.main" }}>
-        <CardActionArea>
-          <CardContent>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
+      {loading && (
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="color-ring-loading"
+          wrapperStyle={{}}
+          wrapperClass="color-ring-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      )}
+      {!loading && (
+        <Card sx={{ maxWidth: 345, backgroundColor: "primary.main" }}>
+          <CardActionArea>
+            <CardContent>
               <Box
                 sx={{
-                  color: "black",
-                  fontSize: "24px",
-                  fontWeight: 700,
+                  display: "flex",
+                  justifyContent: "space-between",
                 }}
               >
-                Lizard
+                <Box
+                  sx={{
+                    color: "black",
+                    fontSize: "24px",
+                    fontWeight: 700,
+                  }}
+                >
+                  {message?.subject}
+                </Box>
               </Box>
-            </Box>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species...
-            </Typography>
-            <Box
-              sx={{
-                paddingTop: "30px",
-              }}
-            >
-              fkfkffk
-            </Box>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+              <Typography variant="body2" color="text.secondary">
+                {message?.content}
+              </Typography>
+              <Box
+                sx={{
+                  paddingTop: "30px",
+                }}
+              >
+                {message?.sender}
+              </Box>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      )}
     </Box>
   );
 }
